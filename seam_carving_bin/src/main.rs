048110e1ea58780f2b4bbd4bf81a51f5afcc90ba
Leonardo Kenji <leokk06@gmail.com>
2024-12-14 21:35:57 -0300

    Add a draft of a cli

--- seam_carving_bin/src/main.rs
+++ seam_carving_bin/src/main.rs
@@ -1,21 +1,27 @@
-use bpaf::Bpaf;
+// // use bpaf::Bpaf;
+use bpaf::*;
 use seam_carving_lib::open_image;
-use std::{error::Error, ffi::OsString, path::PathBuf};
+use std::{error::Error, path::PathBuf};
 
 #[derive(Debug, Clone, Bpaf)]
 #[bpaf(options)]
 pub struct CliOptions {
-    /// Path for the image that will be carved.
-    image_path: OsString,
-    /// Number of columsns to be carved.
-    n_cols: u32,
     /// Path for the output image.
-    out_image: Option<OsString>,
+    out_image: Option<PathBuf>,
+
+    /// Path for the image that will be carved.
+    #[bpaf(positional("IMAGE PATH"), fallback(PathBuf::from("Castle5565.jpg")))]
+    image_path: PathBuf,
+
+    /// Number of columns to be carved.
+    #[bpaf(positional("NUMBER OF COLUMNS TO BE REMOVED"), fallback(10))]
+    n_cols: u32,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let image_path = PathBuf::from("Castle5565.jpg");
+    let cli_options = cli_options().run();
+    let image_path = cli_options.image_path;
     let original_img = open_image(image_path.as_path())?;
-    seam_carving_lib::execute(original_img, 100);
+    seam_carving_lib::execute(original_img, cli_options.n_cols);
     Ok(())
 }
